c751b6b71166d4a4720a46d7d650032aeab570f5
devfernandoabreu <devfernandoabreu@gmail.com>
2021-01-15 17:33:49 -0300

    Front-end Gobarber

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,59 @@
+import React, { InputHTMLAttributes } from 'react';
+import { IconBaseProps } from 'react-icons';
+import { FiAlertCircle } from 'react-icons/fi';
+import { useField } from '@unform/core';
+
+import { Container, Error } from './styles';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  icon?: React.ComponentType<IconBaseProps>;
+}
+
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  const [isFocused, setIsFocused] = React.useState(false);
+  const [isFilled, setIsFilled] = React.useState(false);
+
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  const handleInputFocus = React.useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = React.useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
+
+  React.useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container isErrored={!!error} isFocused={isFocused} isFilled={isFilled}>
+      {Icon && <Icon size={20} />}
+      <input
+        onBlur={handleInputBlur}
+        onFocus={handleInputFocus}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
+
+      {error && (
+        <Error title={error}>
+          <FiAlertCircle color="#c53030" size={20} />
+        </Error>
+      )}
+    </Container>
+  );
+};
+
+export default Input;
