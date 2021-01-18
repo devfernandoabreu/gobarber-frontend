9950a4b444784a97b3245a23582bc4557e57abc0
devfernandoabreu <devfernandoabreu@gmail.com>
2021-01-18 10:01:49 -0300

    Criando calendário

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import DayPicker, { DayModifiers } from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
 import {
   Container,
@@ -9,6 +11,7 @@ import {
   Schedule,
   NextAppointment,
   Section,
+  Appointment,
   Calendar,
 } from './styles';
 
@@ -17,6 +20,17 @@ import { FiClock, FiPower } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = React.useState(new Date());
+
+  const handleDateChange = React.useCallback(
+    (day: Date, modifiers: DayModifiers) => {
+      if (modifiers.available) {
+        setSelectedDate(day);
+      }
+    },
+    [],
+  );
+
   const { signOut, user } = useAuth();
 
   return (
@@ -62,17 +76,88 @@ const Dashboard: React.FC = () => {
                 08:00
               </span>
             </div>
-
-            <Section>
-              <strong>Manhã</strong>
-            </Section>
-
-            <Section>
-              <strong>Tarde</strong>
-            </Section>
           </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/48420092?s=460&u=7a83e3e5782a85872885697596c0360d85712dca&v=4"
+                  alt="Fernando"
+                />
+                <strong>Fernando Abreu</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/48420092?s=460&u=7a83e3e5782a85872885697596c0360d85712dca&v=4"
+                  alt="Fernando"
+                />
+                <strong>Fernando Abreu</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/48420092?s=460&u=7a83e3e5782a85872885697596c0360d85712dca&v=4"
+                  alt="Fernando"
+                />
+                <strong>Fernando Abreu</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
-        <Calendar />
+
+        <Calendar>
+          <DayPicker
+            weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+            fromMonth={new Date()}
+            disabledDays={[{ daysOfWeek: [0, 6] }]}
+            modifiers={{
+              available: { daysOfWeek: [1, 2, 3, 4, 5] },
+            }}
+            selectedDays={selectedDate}
+            onDayClick={handleDateChange}
+            months={[
+              'Janeiro',
+              'Fevereiro',
+              'Março',
+              'Abril',
+              'Maio',
+              'Junho',
+              'Julho',
+              'Agosto',
+              'Setembro',
+              'Outubro',
+              'Novembro',
+              'Dezembro',
+            ]}
+          />
+        </Calendar>
       </Content>
     </Container>
   );
